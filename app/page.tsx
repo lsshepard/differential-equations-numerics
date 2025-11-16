21ecd8214cc4cb9b2f60fec29877c896be059e67
lsshepard <liam.robert.shepard@gmail.com>
2025-11-16 17:21:17 +0100

    newton interpolation

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,10 @@ import dynamic from "next/dynamic";
 const Plot = dynamic(() => import("react-plotly.js"), { ssr: false });
 
 import { linspace } from "@/lib/interpolation/utils";
-import lagrange_interpolation from "@/lib/interpolation/interpolation";
+import {
+  lagrange_interpolation,
+  newton_interpolation,
+} from "@/lib/interpolation/interpolation";
 
 export default function Home() {
   const [a, setA] = useState(-4);
@@ -13,24 +16,27 @@ export default function Home() {
   const [N, setN] = useState(5);
 
   const M = 1000;
-  const f = (x: number) => 2.5 * Math.sin(x);
+  // const f = (x: number) => x ** 3;
+  const f = (x: number) => 2.5 * Math.sin(x) + 2 * x;
 
   const safeN = Math.max(2, N); // avoid degenerate interpolation
 
   const x_points = linspace(a, b, safeN);
   const y_points = x_points.map((v) => f(v));
 
-  const f_interpolation = lagrange_interpolation(x_points, y_points);
+  const f_interpolation_lagrange = lagrange_interpolation(x_points, y_points);
+  const f_interpolation_newton = newton_interpolation(x_points, y_points);
 
   const original_x = linspace(a, b, M);
   const original_y = original_x.map((v) => f(v));
 
   const interp_x = linspace(a, b, M);
-  const interp_y = interp_x.map((v) => f_interpolation(v));
+  const lagrange_interp_y = interp_x.map((v) => f_interpolation_lagrange(v));
+  const newton_interp_y = interp_x.map((v) => f_interpolation_newton(v));
 
   return (
-    <div style={{ padding: 16 }}>
-      <div style={{ display: "flex", gap: 12, marginBottom: 16 }}>
+    <div className="flex items-start m-10">
+      <div className="flex flex-col gap-2 border-2 border-gray-400 rounded-2xl p-2">
         <label>
           a:{" "}
           <input
@@ -75,14 +81,23 @@ export default function Home() {
           },
           {
             x: interp_x,
-            y: interp_y,
+            y: lagrange_interp_y,
             mode: "lines",
-            name: "interpolation",
-            line: { color: "blue" },
+            name: "Lagrange interpolation",
+            line: { color: "blue", dash: "dot" },
+            // opacity: 0.5,
+          },
+          {
+            x: interp_x,
+            y: newton_interp_y,
+            mode: "lines",
+            name: "Newton interpolation",
+            line: { color: "red", dash: "dash" },
+            // opacity: 0.5,
           },
         ]}
         layout={{ margin: { t: 20, r: 20, b: 40, l: 40 } }}
-        style={{ width: "100%", height: "500px" }}
+        style={{ width: "100%", height: "500px", flex: 1 }}
       />
     </div>
   );
--- lib/interpolation/interpolation.ts
+++ lib/interpolation/interpolation.ts
@@ -1,9 +1,9 @@
 const g = (x: number) => x ** 2;
 
-function lagrange_interpolation(
+export const lagrange_interpolation = (
   X: number[],
   Y: number[]
-): (x: number) => number {
+): ((x: number) => number) => {
   const l: ((x: number) => number)[] = [];
   for (let i = 0; i < X.length; i++) {
     let l_i = (x: number) => Y[i];
@@ -23,6 +23,36 @@ function lagrange_interpolation(
   };
 
   return interpolation;
-}
+};
 
-export default lagrange_interpolation;
+export const newton_interpolation = (X: number[], Y: number[]) => {
+  const divided_differences: number[][] = Array.from({ length: Y.length }, () =>
+    Array(Y.length).fill(0)
+  );
+  divided_differences[0] = [...Y];
+
+  let w = (x: number) => 1;
+  const p: ((x: number) => number)[] = [(x: number) => Y[0]];
+
+  for (let i = 1; i < Y.length; i++) {
+    for (let j = i; j < Y.length; j++) {
+      divided_differences[i][j] =
+        (divided_differences[i - 1][j] - divided_differences[i - 1][j - 1]) /
+        (X[j] - X[j - i]);
+    }
+    const old_w = w;
+    w = (x: number) => old_w(x) * (x - X[i - 1]);
+    const w_i = w;
+    p.push((x: number) => w_i(x) * divided_differences[i][i]);
+  }
+
+  console.log(divided_differences);
+
+  const interpolation = (x: number) => {
+    let y = 0;
+    for (const p_i of p) y += p_i(x);
+    return y;
+  };
+
+  return interpolation;
+};
